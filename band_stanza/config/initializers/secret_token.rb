b606eb90fb489e9dad3b59a7731a330858f8f91e
fender707 <fender707@rambler.ru>
2014-04-13 18:12:25 +0300

    13.04 - Project create

---
+++ band_stanza/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BandStanza::Application.config.secret_token = 'bbc36b7e02398313a701b936e1fc35a76dbc93bdf67e8b31a2fe939a6b16fce826f1f0cd1bea193b7efb9f701b37b5d798e8e6ac07fe7952ad08c7ac6ec0f540'
