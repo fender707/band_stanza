5424d459453a75b3ea4cfab01099328e947dbbdd
fender707 <fender707@rambler.ru>
2014-04-29 15:46:22 +0300

    Added Sign in, sign out with Facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+private
+  def current_human
+    @current_human ||= Human.find(session[:human_id]) if session[:human_id]
+  end
+  helper_method :current_human
 end
